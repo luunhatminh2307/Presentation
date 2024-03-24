75b0c0e2a87a1790b64cba1f81daeb97696b64d9
Lưu Thoại Phát <luunhatminh2307@gmai.com>
2024-03-24 20:41:38 +0900

    *kaiho*

--- CLR/Sample/SampleCLI/SampleCLI.h
+++ CLR/Sample/SampleCLI/SampleCLI.h
@@ -7,29 +7,50 @@ using namespace System::Runtime::CompilerServices;
 
 
 namespace SampleCLI {
+	//SampleDLL.dll内の関数を提供するためのマネージドクラス
 	[Extension]
     public ref class Math
 	{
 	public:
-		//DLLのSumFunction()のラッパー関数
+		//SampleDLL.dllのSumFunction()のラッパー関数
 		static int SumFunction_CLI(int a, int b)
 		{
 			return SampleDLL::SumFunction(a, b);
 		}
 	};
 
-	//Calculatorのラッパークラスの作成
+	//SampleDLL.dllのCalculatorクラスのマネージドラッパークラス
 	public ref class Caculator_CLI
 	{
+		//ネイティブCalculatorクラスのポインタ
 		SampleDLL::Calculator* m_calculator;
+
 	public :
+		// コンストラクター
 		Caculator_CLI() {
+			// ネイティブのCalculatorインスタンスの生成
 			m_calculator = new SampleDLL::Calculator();
 		}
+
+		//デストラクター
 		~Caculator_CLI() {
-			delete m_calculator;
+			//ここはマネージリソースを解放
+			//（現在はマネージリソースがないため、何もしない）
+			
+			//次にアンマネージリソースの解放をファイナライザー呼び出し
+			this->!Caculator_CLI();
 		}
 
+		//ファイナライザー
+		!Caculator_CLI() {
+			//アンマネージリソース(ネイティブ)を解放
+			if (m_calculator != nullptr) {
+				delete m_calculator;
+				m_calculator = nullptr;
+			}
+		}
+
+		//SumMethod()のラッパー関数
 		int SumMethod(int a,int b)
 		{
 			return m_calculator->SumMethod(a, b);
